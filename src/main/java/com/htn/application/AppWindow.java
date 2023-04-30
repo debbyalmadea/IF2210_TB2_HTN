54ff98e56fb25863bb757bc68e1c59292a50743d
debbyalmadea <almadeaputri@gmail.com>
2023-04-30 15:54:50 +0700

    add: load font

--- src/main/java/com/htn/application/AppWindow.java
+++ src/main/java/com/htn/application/AppWindow.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import lombok.Getter;
 
@@ -16,6 +17,12 @@ public class AppWindow extends Application {
     @Getter private TabPane tabPane;
     @Override
     public void start(Stage stage) throws Exception {
+        // load font
+        Font.loadFont(getClass().getResource("/font/Inter-Bold.ttf").toExternalForm(), 14);
+        Font.loadFont(getClass().getResource("/font/Inter-Regular.ttf").toExternalForm(), 14);
+        Font.loadFont(getClass().getResource("/font/Inter-Medium.ttf").toExternalForm(), 14);
+
+
         stage.setTitle("HTN development");
         tabPane = new TabPane();
         appMenu = new AppMenu(tabPane);
