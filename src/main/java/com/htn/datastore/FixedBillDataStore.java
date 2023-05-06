ce6f05b6a34eb024aab1b0892b8a431044743539
Mifkiyan <m.favian199@gmail.com>
2023-05-06 19:04:53 +0700

    feat: edit bill data store

--- src/main/java/com/htn/datastore/FixedBillDataStore.java
+++ src/main/java/com/htn/datastore/FixedBillDataStore.java
@@ -2,7 +2,6 @@ package com.htn.datastore;
 
 import com.google.gson.reflect.TypeToken;
 import com.htn.data.bill.FixedBill;
-import com.htn.data.customer.Customer;
 import com.htn.data.item.Item;
 import com.htn.datastore.utils.*;
 import lombok.Getter;
@@ -11,13 +10,14 @@ import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class FixedBillDataStore {
     @Getter
-    private ArrayList<FixedBill> bills;
+    private ArrayList<FixedBill> fixedBills;
     private static FixedBillDataStore instance = null;
     @Getter @Setter
-    private String filename = "bills.json";
+    private String file = "fixed_bill.json";
     private FixedBillDataStore() {
         read();
     }
@@ -31,28 +31,28 @@ public class FixedBillDataStore {
     public void read() {
         Type type = new TypeToken<ArrayList<FixedBill>>() {}.getType();
         IFileReader JSONreader = new JSONUtil(type);
-        Object result = JSONreader.readFile(filename);
-        bills = (ArrayList<FixedBill>) result;
+        Object result = JSONreader.readFile(file);
+        fixedBills = (ArrayList<FixedBill>) result;
     }
 
     public void write() {
         Type type = new TypeToken<ArrayList<FixedBill>>() {}.getType();
         IDataWriter JSONwriter = new JSONUtil(type);
-        JSONwriter.writeData(filename, bills);
+        JSONwriter.writeData(file, fixedBills);
     }
 
-    public void addNew(@NotNull Customer customer, @NotNull double price, @NotNull ArrayList<Item> items) {
-        bills.add(new FixedBill(customer, price, items));
+    public void addNew(@NotNull int id, @NotNull String name, @NotNull double price, @NotNull Date date, @NotNull ArrayList<Item> items) {
+        fixedBills.add(new FixedBill(id, name, price, date, items));
         write();
     }
 
     public void addNew(FixedBill bill) {
-        bills.add(bill);
+        fixedBills.add(bill);
         write();
     }
 
     public void remove(FixedBill bill) {
-        bills.remove(bill);
+        fixedBills.remove(bill);
         write();
     }
 }
